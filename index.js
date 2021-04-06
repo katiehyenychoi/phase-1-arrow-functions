8ae2bbc965e8ed225a180a5a9840ea29470b49e0
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-04-05 23:11:17 -0400

    commit tree 4f4121235222896804e86ea4547bd3ad48304944

---
+++ index.js
@@ -0,0 +1,14 @@
+//has a function expression called divide
+//divide divides 2000 by 100
+
+function divide() {
+    let res = (2000 / 100);
+    return res;
+};
+
+
+
+const square = x => x * x;
+const add = (x, y) => x + y;
+
+
